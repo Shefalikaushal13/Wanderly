0a6313f1e49a4fc7355861665a4f5102c78ed804
Shefali Kaushal <shefali2404kaushal@gmail.com>
2024-09-27 20:31:02 +0530

    Updated Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ export default function Footer() {
     <footer className=" bg-gray-800 text-white py-6">
       <div className="container mx-auto text-center">
         <div className="flex justify-center space-x-7">
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
-            <i className="fab fa-facebook-f"></i>
+          <a href="https://github.com/Shefalikaushal13" target="_blank" rel="noopener noreferrer" className="hover:text-black">
+            <i className="fa-brands fa-github"></i>
           </a>
           <a href="https://x.com/shefaliu_u" target="_blank" rel="noopener noreferrer" className="hover:text-black">
             <i className="fab fa-x"></i>
